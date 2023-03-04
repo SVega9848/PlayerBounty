c8eb8e29ffd0111e0d748a15316e4294b77d0f3e
SVega9848 <108686235+SVega9848@users.noreply.github.com>
2023-03-04 23:22:32 +0100

    Add files via upload

---
+++ src/PlayerBounty/Commands/PlayerBountyCommand.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace PlayerBounty\Commands;
+
+use PlayerBounty\Loader;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\utils\CommandException;
+use pocketmine\lang\Translatable;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+
+class PlayerBountyCommand extends Command {
+
+    private Loader $loader;
+
+    public function __construct(Loader $loader) {
+        parent::__construct("playerbounty", "Set a kill bounty to a player and earn money!");
+        $this->loader = $loader;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if ($sender instanceof Player) {
+            if(isset($args[0])) {
+                switch($args[0]) {
+                    case "set":
+                        if(isset($args[1])) {
+                            if($this->loader->getServer()->getPlayerByPrefix($args[1])->getName() == $sender->getName()) {
+                                $sender->sendMessage(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("same-name-error"), [
+                                    "PREFIX" => $this->loader->config->get("prefix")
+                                ])));
+                            } else {
+                                if((int)$args[2] > $this->loader->getMoney($sender)) {
+                                    $sender->sendMessage(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("not-enough-money"), [
+                                        "PREFIX" => $this->loader->config->get("prefix")
+                                    ])));
+                                } else {
+                                    if($this->loader->getServer()->getPlayerByPrefix($args[1])->isOnline()) {
+                                        if (!$this->loader->isBountyList($this->loader->getServer()->getPlayerByPrefix($args[1])->getName())) {
+                                            $this->loader->addBountyList($this->loader->getServer()->getPlayerByPrefix($args[1])->getName(), (int)$args[2]);
+                                            $this->loader->getServer()->broadcastMessage(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("broadcast-bounty"), [
+                                                "PREFIX" => $this->loader->config->get("prefix"),
+                                                "BETTOR" => $sender->getName(),
+                                                "PLAYER" => $this->loader->getServer()->getPlayerByPrefix($args[1])->getName(),
+                                                "MONEY" => $args[2]
+                                            ])));
+                                            $this->loader->removeMoney($sender, $args[2]);
+                                        } else {
+                                            $sender->sendMessage(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("player-already-bounty"), [
+                                                "PREFIX" => $this->loader->config->get("prefix")
+                                            ])));
+                                        }
+                                    } else {
+                                        $sender->sendMessage(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("player-not-online"), [
+                                            "PREFIX" => $this->loader->config->get("prefix")
+                                        ])));
+                                    }
+                                }
+                            }} else {
+                            $sender->sendMessage(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("usage-message"), [
+                                "PREFIX" => $this->loader->config->get("prefix")
+                            ])));
+                        }
+                        break;
+                    case "list":
+                        if(count($this->loader->bounty) > 0) {
+                            $this->bountyList($sender);
+                        } else {
+                            $sender->sendMessage(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("not-enough-players-bounty"), [
+                                "PREFIX" => $this->loader->config->get("prefix")
+                            ])));
+                        }
+                        break;
+                }
+
+            } else {
+                $sender->sendMessage(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("usage-message"), [
+                    "PREFIX" => $this->loader->config->get("prefix")
+                ])));
+            }
+        }
+    }
+
+    public function bountyList(Player $player) {
+
+        $form =  new \jojoe77777\FormAPI\SimpleForm(function (Player $player, int $data = null){
+            return true;
+        });
+        $form->setTitle(TextFormat::colorize($this->loader->getConfigMessage("prefix")));
+        $bountylist = $this->loader->bounty;
+        foreach($bountylist as $key => $index) {
+            $form->addButton(TextFormat::colorize($this->loader->replaceVars($this->loader->config->get("bountyUI-list"), [
+                "PLAYER" => strval($key),
+                "MONEY" => strval($index)
+            ])));
+        }
+        $player->sendForm($form);
+    }
+
+}
---
+++ src/PlayerBounty/Loader.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PlayerBounty;
+
+use PlayerBounty\Events\Events;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\event\Listener;
+use pocketmine\event\server\CommandEvent;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use PlayerBounty\Commands\PlayerBountyCommand;
+use pocketmine\utils\Config;
+use function array_shift;
+
+class Loader extends PluginBase {
+
+    public EconomyAPI $economy;
+    public Config $config;
+    public array $bounty = [];
+
+	public function onEnable() : void{
+        $this->saveResource("messages.yml");
+        $this->config = new Config($this->getDataFolder(). "/messages.yml", Config::YAML);
+        $this->economy = EconomyAPI::getInstance();
+		$this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
+        $this->getServer()->getCommandMap()->register("playerbounty", new PlayerBountyCommand($this));
+	}
+
+    public function getMoney(Player $player) : float {
+        $money = $this->economy->myMoney($player->getName());
+        assert(is_float($money));
+        return $money;
+    }
+
+    public function addMoney(Player $player, float $money) : void {
+        $this->economy->addMoney($player->getName(), $money);
+    }
+
+    public function removeMoney(Player $player, float $money) : void {
+        $this->economy->reduceMoney($player->getName(), $money);
+    }
+
+    public function getConfigMessage(string $path) : string {
+        $config = new Config($this->getDataFolder(). "/messages.yml", Config::YAML);
+        return $config->get("$path");
+    }
+
+    public function replaceVars(string $str, array $vars): string {
+        foreach ($vars as $key => $value) {
+            $str = str_replace("{" . $key . "}", $value, $str);
+        }
+        return $str;
+    }
+
+    public function isBountyList(string $name) : bool {
+        return array_key_exists($name, $this->bounty);
+    }
+
+    public function addBountyList(string $name, int $amount) {
+        $this->bounty[$name] = $amount;
+    }
+
+    public function deleteBountylist(string $name) {
+        unset($this->bounty[$name]);
+    }
+
+}
